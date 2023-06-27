bfc3558051155a7aeca759207863461dc8d2615e
Vova <uladzimirfilipau@gmail.com>
2023-06-27 21:33:05 +0300

    feat: About

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import { About } from './pages/About';
 import { Navbar } from './components/Navbar';
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <div className='container pt-4'>
+        <Routes>
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </div>
     </>
   );
 }
---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export const About = () => (
+  <div className='card w-100 text-bg-light text-center'>
+    <div className='card-body'>
+      <h1 className='card-title'>Best React Todo App</h1>
+      <p className='card-text'>Version 1.0</p>
+    </div>
+  </div>
+);
