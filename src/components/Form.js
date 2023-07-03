6a181bb7ccfce501b5990c22a85086906890b640
Vova <uladzimirfilipau@gmail.com>
2023-07-03 09:13:20 +0300

    refactor: Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,24 @@
 import { useContext, useState } from 'react';
 import { AlertContext } from '../context/alert/alertContext';
+import { FirebaseContext } from '../context/firebase/firebaseContext';
 
 export const Form = () => {
   const [value, setValue] = useState('');
-
   const alert = useContext(AlertContext);
+  const firebase = useContext(FirebaseContext);
 
   const submitHandler = (event) => {
     event.preventDefault();
 
     if (value.trim()) {
-      alert.show('Todo was created', 'success');
+      firebase
+        .addNote(value.trim())
+        .then(() => {
+          alert.show('Todo was created', 'success');
+        })
+        .catch(() => {
+          alert.show('Todo not created', 'alert');
+        });
       setValue('');
     } else {
       alert.show('Please enter todo name');
