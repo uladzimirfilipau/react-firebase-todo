253c8b3f6ddf65bb1291e77e02ab8fca16f1f8dc
Vova <uladzimirfilipau@gmail.com>
2023-07-05 06:34:26 +0300

    feat: AlertState

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { AlertState } from './context/alert/AlertState';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AlertState>
   </React.StrictMode>,
 );
