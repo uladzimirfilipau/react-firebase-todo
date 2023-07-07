edf17e2905f8dac8ab95a0be99a486e035f5a10d
Vova <uladzimirfilipau@gmail.com>
2023-07-07 08:55:55 +0300

    refactor: Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,24 @@
-import { Fragment } from 'react';
+import { Fragment, useContext, useEffect } from 'react';
 import { Form } from '../components/Form';
 import { Notes } from '../components/Notes';
+import { FirebaseContext } from '../context/firebase/firebaseContext';
+import { Loader } from '../components/Loader';
 
 export const Home = () => {
+  const { loading, notes, fetchNotes } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    fetchNotes();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <Fragment>
       <Form />
+
       <hr></hr>
-      <Notes notes={notes} />
+
+      {loading ? <Loader /> : <Notes notes={notes} />}
     </Fragment>
   );
 };
