c21c37000f5fb9d830ac7c1aed6ec5ee706f5f84
Vova <uladzimirfilipau@gmail.com>
2023-07-14 11:05:14 +0300

    refactor: Note

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,11 +2,13 @@ import { useContext, useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import { FirebaseContext } from '../context/firebase/firebaseContext';
 import { AlertContext } from '../context/alert/alertContext';
+import { useConfirm } from '../context/confirm/useConfirm';
 
 export const Note = ({ note, onRemove, ...rest }) => {
   const nodeRef = useRef(null);
   const firebase = useContext(FirebaseContext);
   const alert = useContext(AlertContext);
+  const { confirm } = useConfirm();
 
   const removeHandler = () => {
     firebase
@@ -19,6 +21,14 @@ export const Note = ({ note, onRemove, ...rest }) => {
       });
   };
 
+  const showConfirm = async () => {
+    const isConfirmed = await confirm('Do you confirm delete?');
+
+    if (isConfirmed) {
+      removeHandler();
+    }
+  };
+
   return (
     <CSSTransition {...rest} nodeRef={nodeRef} classNames={'note'} timeout={800}>
       <li className='list-group-item note' ref={nodeRef}>
@@ -26,7 +36,7 @@ export const Note = ({ note, onRemove, ...rest }) => {
           <strong>{note.title}</strong>
           <span>{note.date}</span>
         </div>
-        <button type='button' className='btn btn-outline-danger btn-sm' onClick={removeHandler}>
+        <button type='button' className='btn btn-outline-danger btn-sm' onClick={showConfirm}>
           &times;
         </button>
       </li>
